6e7cf55d6e41f1068fe4e0aa71467cb30c5a7a54
David Lynch <kemayo@gmail.com>
2022-09-19 20:04:53 -0500

    Initial version: rares

---
+++ wrath.lua
@@ -0,0 +1,384 @@
+local myname, ns = ...
+
+-- ns.points = {}
+
+-- local merge = function(t1, t2)
+--     if not t2 then return t1 end
+--     for k, v in pairs(t2) do
+--         t1[k] = v
+--     end
+--     return t1
+-- end
+-- ns.RegisterPoints = function(uiMapID, points, default)
+--     if not ns.points[uiMapID] then ns.points[uiMapID] = {} end
+--     for coord, point in pairs(points) do
+--         merge(point, default)
+--         ns.points[uiMapID][coord] = point
+--     end
+-- end
+
+-- Borean Tundra
+ns.RegisterPoints(114, {
+    [21002840] = {},
+    [22003340] = {},
+    [27003560] = {},
+    [34002420] = {},
+    [35402940] = {},
+}, { -- Old Crystalbark
+    achievement=2257,
+    criteria=8103,
+    npc=32357,
+    loot={44649},
+})
+ns.RegisterPoints(114, {
+    [59801460] = {},
+    [59802540] = {},
+    [62603480] = {},
+    [68001920] = {},
+    [68403660] = {},
+    [73603260] = {},
+}, { -- Fumblub Gearwind
+    achievement=2257,
+    criteria=8104,
+    npc=32358,
+})
+ns.RegisterPoints(114, {
+    [80404600] = {},
+    [81203160] = {},
+    [88203960] = {},
+    [91403240] = {},
+}, { -- Icehorn
+    achievement=2257,
+    criteria=8105,
+    npc=32361,
+    loot={44667},
+})
+
+-- Howling Fjord
+ns.RegisterPoints(117, {
+    [49800460] = {},
+    [52801160] = {},
+    [60802000] = {},
+    [68201720] = {},
+}, { -- Perobas the Bloodthirster
+    achievement=2257,
+    criteria=8106,
+    npc=32377,
+    loot={44669},
+})
+ns.RegisterPoints(117, {
+    [68604840] = {},
+    [69405820] = {},
+    [73403980] = {},
+    [74004500] = {},
+    [74005240] = {},
+    [74406060] = {},
+}, { -- Vigdis the War Maiden
+    achievement=2257,
+    criteria=8107,
+    npc=32386,
+    loot={44670},
+})
+ns.RegisterPoints(117, {
+    [26006380] = {},
+    [30807120] = {},
+    [31205660] = {},
+    [33208020] = {},
+}, { -- King Ping
+    achievement=2257,
+    criteria=8108,
+    npc=32398,
+    loot={44668},
+})
+
+-- Dragonblight
+ns.RegisterPoints(115, {
+    [54405540] = {},
+    [57004980] = {},
+    [59204240] = {},
+    [60603020] = {},
+    [61805660] = {},
+    [62605100] = {},
+    [66803280] = {},
+    [68004600] = {},
+    [68805780] = {},
+    [70005140] = {},
+}, { -- Tukemuth
+    achievement=2257,
+    criteria=8109,
+    npc=32400,
+    loot={44673},
+})
+ns.RegisterPoints(115, {
+    [15405820] = {},
+    [15604560] = {},
+    [20605520] = {},
+    [28406140] = {},
+}, { -- Crazed Indu'le Survivor
+    achievement=2257,
+    npc=32409,
+    loot={44672},
+})
+ns.RegisterPoints(115, {
+    [69207480] = {},
+    [71002220] = {},
+    [75202760] = {},
+    [85803660] = {},
+}, { -- Scarlet Highlord Daion
+    achievement=2257,
+    criteria=8111,
+    npc=32417,
+})
+
+-- Grizzly Hills
+ns.RegisterPoints(116, {
+    [10603920] = {},
+    [11207100] = {},
+    [12005560] = {},
+    [12204440] = {},
+    [12805000] = {},
+    [17207040] = {},
+    [20605620] = {},
+    [22407320] = {},
+    [28004180] = {},
+}, { -- Grocklar
+    achievement=2257,
+    criteria=8112,
+    npc=32422,
+    loot={44675},
+})
+ns.RegisterPoints(116, {
+    [28004540] = {},
+    [34004920] = {},
+    [39605060] = {},
+}, { -- Seething Hate
+    achievement=2257,
+    criteria=8113,
+    npc=32429,
+})
+ns.RegisterPoints(116, {
+    [62803700] = {},
+    [65202940] = {},
+    [71603500] = {},
+    [74204240] = {},
+}, { -- Syreian the Bonecarver
+    achievement=2257,
+    criteria=8114,
+    npc=32438,
+})
+ns.RegisterPoints(116, {
+    [31005580] = { -- Arcturis
+        npc=38453,
+        loot={51958},
+    },
+})
+
+-- Zul'drak
+ns.RegisterPoints(121, {
+    [21208260] = {},
+    [26208280] = {},
+    [28807220] = {},
+    [40405460] = {},
+    [40406000] = {},
+    [42607060] = {},
+    [45806040] = {},
+    [45807580] = {},
+    [49808240] = {},
+}, { -- Zul'drak Sentinel
+    achievement=2257,
+    criteria=8115,
+    npc=32447,
+    loot={44677},
+})
+ns.RegisterPoints(121, {
+    [14405620] = {},
+    [17407020] = {},
+    [20807880] = {},
+    [22406180] = {},
+    [26205560] = {},
+    [26607100] = {},
+}, { -- Griegen
+    achievement=2257,
+    criteria=8116,
+    npc=32471,
+    loot={44686},
+})
+ns.RegisterPoints(121, {
+    [53203140] = {},
+    [61203640] = {},
+    [71402320] = {},
+    [71402900] = {},
+    [74406640] = {},
+    [77204220] = {},
+    [81403440] = {},
+}, { -- Terror Spinner
+    achievement=2257,
+    criteria=8117,
+    npc=32475,
+    loot={44685},
+})
+ns.RegisterPoints(121, {
+    [61006160] = {},
+    [63204340] = {},
+    [67607740] = {},
+    [69404800] = {},
+    [77006940] = {},
+}, { -- Gondria
+    npc=33776,
+    loot={46324},
+})
+
+-- Sholazar Basin
+ns.RegisterPoints(119, {
+    [40205900] = {},
+    [41206840] = {},
+    [42007380] = {},
+    [42205180] = {},
+    [52407240] = {},
+    [54405180] = {},
+    [56406520] = {},
+}, { -- Aotona
+    achievement=2257,
+    criteria=8118,
+    npc=32481,
+})
+ns.RegisterPoints(119, {
+    [25804880] = {},
+    [29204220] = {},
+    [32603540] = {},
+    [36202960] = {},
+    [47004340] = {},
+    [50808140] = {},
+    [52204240] = {},
+    [58808180] = {},
+    [63808280] = {},
+}, { -- King Krush
+    achievement=2257,
+    criteria=8119,
+    npc=32485,
+    loot={44683},
+})
+ns.RegisterPoints(119, {
+    [20607000] = {},
+    [30806640] = {},
+    [35402960] = {},
+    [50808120] = {},
+    [58402140] = {},
+    [66007900] = {},
+    [70807120] = {},
+}, { -- Loque'nahak
+    achievement=2257,
+    criteria=8100,
+    npc=32517,
+    loot={44687,44688},
+})
+
+-- Icecrown
+ns.RegisterPoints(118, {
+    [44005820] = {},
+    [45205020] = {},
+    [49004280] = {},
+    [54004120] = {},
+    [60804120] = {},
+    [65004740] = {},
+    [66205260] = {},
+    [67405820] = {},
+    [68406420] = {},
+}, { -- Putridus the Ancient
+    achievement=2257,
+    criteria=8122,
+    npc=32487,
+    loot={44696},
+})
+ns.RegisterPoints(118, {
+    [28604540] = {},
+    [29603800] = {},
+    [30803280] = {},
+    [36802540] = {},
+    [54005300] = {},
+    [59605940] = {},
+}, { -- Hildana Deathstealer
+    achievement=2257,
+    criteria=8102,
+    npc=32495,
+    loot={44697},
+})
+ns.RegisterPoints(118, {
+    [31206220] = {},
+    [33607060] = {},
+    [47407820] = {},
+    [48408500] = {},
+    [67603860] = {},
+    [72803500] = {},
+}, { -- High Thane Jorfus
+    achievement=2257,
+    criteria=8101,
+    npc=32501,
+    loot={44695},
+})
+
+-- Storm Peaks
+ns.RegisterPoints(120, {
+    [37805840] = {},
+    [41005180] = {},
+    [41404020] = {},
+    [68004760] = {},
+}, { -- Dirkee
+    achievement=2257,
+    criteria=8121,
+    npc=32500,
+    loot={44681,44708},
+})
+ns.RegisterPoints(120, {
+    [31056945] = {},
+    [35657665] = {},
+    [51057005] = {},
+    [52053405] = {},
+}, { -- Vyragosa
+    achievement=2257,
+    criteria=8120,
+    npc=32630,
+    loot={44732},
+})
+ns.RegisterPoints(120, {
+    [27805040] = {},
+    [30206460] = {},
+    [46206440] = {},
+}, { -- Skoll
+    npc=35189,
+    loot={49227},
+})
+ns.RegisterPoints(120, { -- Time-Lost Proto-Drake
+    [31006940] = {},
+    [35607660] = {},
+    [51007000] = {},
+    [52003400] = {},
+    --[[
+        routes={[120]={
+            {
+                52003400, 43803130, 32603690, 27703960, 28505050, 26805830,
+                28706500, 34706500, 40005980, 47205520, 50304870, 46604040,
+                loop=true,
+                r=0.6,g=0,b=0,
+            },
+            {
+                31006940, 27906450, 26405840, 27805020, 37304120, 39804290, 40405870,
+                loop=true,
+                r=0,g=0.6,b=0,
+            },
+            {
+                35607660, 35706610, 29006580, 26607510, 31208030,
+                loop=true,
+                r=0,g=0,b=0.6,
+            },
+            {
+                51007000, 46206050, 41806080, 40106910, 36907250, 38008100, 46708130,
+                loop=true,
+                r=1,g=1,b=0,
+            },
+        }},
+    ]]
+}, {
+    npc=32491,
+    loot={{44168,mount=265}},
+})
